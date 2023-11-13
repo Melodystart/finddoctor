d279522df8e72e67b1361e04c7e286d156abdc9e
Melodystart <start99start@gmail.com>
2023-11-13 16:15:38 +0800

    revise selenium

--- app.py
+++ app.py
@@ -118,10 +118,10 @@ def readReview(inputtext):
     options = Options()
     ua = UserAgent()
     user_agent = ua.random  # 偽裝隨機產生瀏覽器、作業系統
-    options.add_argument('--no-sandbox')
     options.add_argument(f'--user-agent={user_agent}')
     options.add_argument('headless')
     options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('start-maximized')
     options.add_argument("--disable-extensions")
