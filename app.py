580722030c4e3d57428bc51fae8c6139335874f4
Melodystart <start99start@gmail.com>
2023-11-27 14:00:04 +0800

    revise selenium

--- app.py
+++ app.py
@@ -395,7 +395,9 @@ def readJudgment(inputtext, T1, expiredDay):
             driver.close()
             driver.quit()
         except:
-            pass
+            print("司法院資料異常")
+            driver.close()
+            driver.quit()
 
         T2 = time.perf_counter()
         print("司法院好了："+'%s毫秒' % ((T2 - T1)*1000))
